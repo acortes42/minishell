1ef52d1909909f44b6f6cb8c3753c0698d20799e
acortes- <diosdecarton@gmail.com>
2020-07-30 17:24:58 +0200

    Escribe el texto y guarda en un string

--- minishell.c
+++ minishell.c
@@ -1,30 +1,46 @@
 #include "minishell.h"
 
-int obtain_full_line(int fd, char **parseString)
+int obtain_full_line(int fd, abs_struct *base)
 {
-    write(1, "obta\n", 5);
-    return (get_next_line(fd, parseString));
+    char        str[1];
+
+    str[strlen(str)] = '\0';
+    base->string = ft_strdup("");
+    fd = open("history.txt", O_RDWR | O_APPEND);
+    while (read(0, str, sizeof(str)) > 0)
+    {
+        base->string = ft_strjoin(base->string, str);
+        write(fd, str, 1);
+        if (str[0] == '\n')
+            break;
+    }
+    return (0);
 }
 
-int execute (int  exceptionNum,char **parseString)
-{
-    write(1, "exec\n", 5);
-    exceptionNum = 2;
+int execute (abs_struct *base)
+{   
+    if (base->string == "exit")
+        return (1);
     return (0);
 }
 
 int main(int argc, char **argv)
 {
-    char    **parseString;
-    int     exceptionNum;
+    int         fd;
+    abs_struct  *base;
 
-    exceptionNum = 1;
-    write(1, "Init\n", 5);
-    while (exceptionNum == 1)
+    ft_printf("             |||           MINISHELL          ||| \n");
+    ft_printf("             |||------------------------------||| \n\n\n");
+
+    if (!(base = (abs_struct*)calloc(1, sizeof(abs_struct))))
+	  	return (-1);
+    base->exceptionNum = 1;
+    while (base->exceptionNum == 1)
     {
-        write(1, "Init>", 5);
-        obtain_full_line(1, parseString);
-        execute(exceptionNum, parseString);
+        ft_printf("--->");
+        obtain_full_line(fd, base);
+        if (execute(base) == 1)
+            base->exceptionNum = 0;
     }
     (void)argc;
     (void)argv;
--- minishell.h
+++ minishell.h
@@ -2,31 +2,34 @@
 # define MINISHELL_H
 
 # include <stdio.h>
+#include <string.h>
+# include <fcntl.h>
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <strings.h>
+# include <string.h>
 # include <ctype.h>
 # include <stdarg.h>
 
 
 #define BUFFER_SIZE 1024
 
-/*
+
 typedef struct      abs_struct
 {
     char    *string;
-    char    **parseString;
+    char    *parseString;
     int     exceptionNum;
     int     num_args;
 }                   abs_struct;    
-*/
+
 
 char	*ft_strdup(const char *s1);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(char *str, int c);
 char	*ft_strcdup(const char *s1, int c);
 int		get_next_line(int fd, char **line);
+int     ft_printf(const char *arr, ...);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(char const *s, char c);
 #endif
