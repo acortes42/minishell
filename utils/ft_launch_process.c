d14b6c6057caa9bce7d9ae47726bd9082dce1aae
visv <victor.sempere1@gmail.com>
2021-02-14 20:07:55 +0100

    Ajustado fichero to_do.txt * Anotado dónde tocar para completar las redirecciones

--- utils/ft_launch_process.c
+++ utils/ft_launch_process.c
@@ -3,7 +3,7 @@
 static void		ft_execute_absolute_shell_command(abs_struct *base,
 	t_process *p)
 {
-// todo: pendiente de ver si se llama bien
+	// TODO: Eliminar de argv las redirecciones
 	if (execve(p->argv[0], p->argv, base->env) && errno == EACCES)
 	{
 		p->status = 126;
@@ -18,20 +18,13 @@ static void		ft_execute_relative_shell_command(abs_struct *base,
 	char		*path;
 	char		*tmp;
 
-	//	 todo: Tratar el error de path
 	if (!(path = getcwd(0, 0)))
 		return ;
 	tmp = ft_strjoin(path, p->argv[0] + 1);
 	free(path);
 	path = tmp;
-	// TODO: pendiente de ver si se está llamando bien
-	if (execve(path, p->argv + 1, base->env) && errno == EACCES)
-	{
-		p->status = 126;
-	}
+	ft_execute_absolute_shell_command(base, p);
 	free(path);
-	p->completed = 1;
-	ft_putstr_fd(strerror(errno), STDERR_FILENO);
 }
 
 static void		ft_execute_shell_command_using_path(abs_struct *base,
@@ -68,8 +61,10 @@ static void		ft_execute_shell_command_using_path(abs_struct *base,
 	p->status = 1;
 }
 
-static void		prepare_process(t_files_fd files_fd)
+static void		prepare_process(t_process *p, t_files_fd files_fd)
 {
+	// TODO: Analizar p->argv en busca de las redirecciones y ajustarlas, teniendo en cuenta los pipes
+
 	/* Set the handling for job control signals back to the default.  */
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
@@ -98,7 +93,7 @@ static void		prepare_process(t_files_fd files_fd)
 void            ft_launch_process(abs_struct *base, t_process *p,
 	t_files_fd files_fd)
 {
-	prepare_process(files_fd);
+	prepare_process(p, files_fd);
 	if (ft_execute_builtin(base, p))
 	{
 		p->completed = 1;
