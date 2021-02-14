90f6a5042b6db5714bb522a33c84ad4f404f4c74
visv <victor.sempere1@gmail.com>
2021-02-14 19:16:29 +0100

    Mostrando cwd en lugar del prompt fijo

--- srcs/ft_show_prompt.c
+++ srcs/ft_show_prompt.c
@@ -1,8 +1,44 @@
 #include "minishell.h"
 
-void    ft_show_prompt(abs_struct *base)
+static char		*ft_get_cwd(abs_struct *base)
 {
-	(void)base;
-    ft_putstr("\e[92m--->");
-    // TODO: Mostrar directorio actual
+	char		*cwd;
+
+	cwd = getcwd(0, 0);
+	if (!cwd || *cwd == '(')
+	{
+		if (cwd)
+			free (cwd);
+		ft_exit_minishell(base, errno);
+	}
+	return (cwd);
+}
+
+void    		ft_show_prompt(abs_struct *base)
+{
+	char		*cwd;
+	char		*tmp;
+	size_t		len;
+	char		*home;
+
+	cwd = ft_get_cwd(base);
+	home = ft_getenv(base->env, "HOME");
+	if (home)
+		home += 5;
+	len = !home ? 0 : ft_strlen(home);
+	while (len && *(home + len - 1) == '/')
+		len--;
+	if (len && !ft_strncmp(cwd, home, len))
+	{
+		tmp = ft_strdup(cwd + len - 1);		
+		free(cwd);
+		if (!tmp)
+			ft_exit_minishell(base, -1);
+		cwd = tmp;
+		cwd[0] = '~';
+	}
+	ft_putstr("\e[92m");
+	ft_putstr(cwd);
+    ft_putstr("$");
+	free(cwd);
 }
