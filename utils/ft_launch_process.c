6136a1c5fd2e1a6de1a4008bbc784fa5c7d65dd4
visv <victor.sempere1@gmail.com>
2021-04-03 11:01:11 +0200

    Corrección de error de ejecución de comando ' o "
    Ya no muestra un mensaje de error por cada path que encaja, se muestra is a directory

--- utils/ft_launch_process.c
+++ utils/ft_launch_process.c
@@ -20,6 +20,7 @@ static void	ft_execute_absolute_shell_command(t_abs_struct *base,
 		p->status = 126;
 	p->completed = 1;
 	ft_putstr_fd(strerror(errno), STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
 }
 
 static void	ft_execute_relative_shell_command(t_abs_struct *base,
@@ -38,34 +39,64 @@ static void	ft_execute_relative_shell_command(t_abs_struct *base,
 	free(path);
 }
 
+static char	*ft_get_path_to_execute(char *paths, char *cmd)
+{
+	struct stat	statbuf;
+	char		*path;
+	char		*tmp;
+
+	path = ft_split_shell_by(&paths, ":");
+	while (path)
+	{
+		tmp = ft_strlcat_paths(path, cmd);
+		free(path);
+		if (!stat(tmp, &statbuf))
+		{
+			if (((statbuf.st_mode & __S_IFMT) == __S_IFDIR) ||
+				((statbuf.st_mode & __S_IFMT) == __S_IFREG))
+				return (tmp);
+		}
+		free(tmp);
+		path = ft_split_shell_by(&paths, ":");
+	}
+	return (0);
+}
+
 static void	ft_execute_shell_command_using_path(t_abs_struct *base,
 	t_process *p)
 {
 	struct stat	statbuf;
 	char		*paths;
 	char		*path;
-	char		*tmp;
 
 	paths = ft_getenv(base->env, "PATH") + 5;
-	path = ft_split_shell_by(&paths, ":");
-	while (path)
+	path = ft_get_path_to_execute(paths, p->argv[0]);
+	if (!path)
 	{
-		tmp = ft_strlcat_paths(path, p->argv[0]);
-		free(path);
-		if (!stat(tmp, &statbuf))
+		ft_putstr_fd(*p->argv, STDERR_FILENO);
+		ft_putstr_fd(": not found command\n", STDERR_FILENO);
+		p->completed = 1;
+		p->status = 1;
+	}
+	else
+	{
+		stat(path, &statbuf);
+		if ((statbuf.st_mode & __S_IFMT) == __S_IFDIR)
 		{
-			if (*tmp == '/' || !ft_strncmp(tmp, "\"/", 2))
-				ft_execute_absolute_shell_command(base, tmp, p);
+			ft_putstr_fd(*p->argv, STDERR_FILENO);
+			ft_putstr_fd(": is a directory\n", STDERR_FILENO);
+			p->completed = 1;
+			p->status = 1;
+		}
+		else if ((statbuf.st_mode & __S_IFMT) == __S_IFREG)
+		{
+			if (*path == '/' || !ft_strncmp(path, "\"/", 2))
+				ft_execute_absolute_shell_command(base, path, p);
 			else
 				ft_execute_relative_shell_command(base, p);
 		}
-		free(tmp);
-		path = ft_split_shell_by(&paths, ":");
+		free(path);
 	}
-	ft_putstr_fd(*p->argv, STDERR_FILENO);
-	ft_putstr_fd(": not found command\n", STDERR_FILENO);
-	p->completed = 1;
-	p->status = 1;
 }
 
 void	ft_launch_process(t_abs_struct *base, t_process *current)
