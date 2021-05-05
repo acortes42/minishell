b716305f3730c4f9625c9b8be5d181a01457198a
visv <victor.sempere1@gmail.com>
2021-05-05 20:42:52 +0200

    Cargada inicialmente la última línea del archivo de history para evitar que se duplique

--- srcs/ft_init_minishell.c
+++ srcs/ft_init_minishell.c
@@ -20,6 +20,10 @@ static void	ft_init_history(t_abs_struct *base)
 	base->history_lines = ft_file_lines_by_fd(fd);
 	if (fd >= 0)
 		close(fd);
+	fd = ft_open_history(base, O_RDONLY);
+	base->last_history_command = ft_get_file_line_by_fd(fd, base->history_lines - 1);
+	if (fd >= 0)
+		close(fd);
 	if (base->history_lines < 0)
 		ft_exit_minishell(base, -1);
 	base->current_history_line = base->history_lines;
