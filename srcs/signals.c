e60c489fe50b65d62b7467e46c3518929b0fd13f
Adrian Cortes espejo <acortes-@c1r7s4.42madrid.com>
2021-05-08 20:29:18 +0200

    Pequeño arreglo para evitar segfault

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: acortes- <acortes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/23 16:04:49 by acortes-          #+#    #+#             */
-/*   Updated: 2021/04/01 16:50:39 by acortes-         ###   ########.fr       */
+/*   Updated: 2021/05/08 20:27:11 by acortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,11 @@ void	signal_handler(int sig)
 
 	if (sig == SIGINT)
 	{
-		ft_memset(g_base.input_bf, 0, BUFFER_SIZE);
-		ft_clear_input(&g_base.input);
+		if (ft_strlen(g_base.input_bf) > 0)
+		{
+			ft_memset(g_base.input_bf, 0, BUFFER_SIZE);
+			ft_clear_input(&g_base.input);
+		}
 		ft_putstr("\n");
 		ft_show_prompt(&g_base);
 		g_base.last_executed_process_status = 130;
