2767bbb85bc30a948e44e2a10a95e3c88636c155
visv <victor.sempere1@gmail.com>
2021-05-21 12:10:27 +0200

    Ampliado el test para simular 3 comandos separados por pipe: top | echo $? | ls

--- tests/test_pipes_top.c
+++ tests/test_pipes_top.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -36,43 +38,89 @@ int main(int argc, char **argv, char **envp)
 	(void)envp;
 	pid_t	pid1;
 	pid_t	pid2;
+	pid_t	pid3;
 	int		fd[2];
+	int		fd23[2];
+	int		infile;
+	int		outfile;
+	int		errfile;
 
-signal(SIGINT, signals);
-		pipe(fd);
-		pid1 = fork();
-		if (!pid1)
-		{
-			close(fd[STDIN_FILENO]);
-			dup2(fd[STDOUT_FILENO], STDOUT_FILENO);
-			close(fd[STDOUT_FILENO]);
-			char *argv[2];
-			argv[0] = "top";
-			argv[1] = 0;
-			// top
-			printf("Top thread child\n");
-			execve("/usr/bin/top", argv, envp);
-			exit(1);
-		}
+	infile = dup(STDIN_FILENO);
+	outfile = dup(STDOUT_FILENO);
+	errfile = dup(STDERR_FILENO);
 
-		pid2 = fork();
-		if (pid2)
-		{
-			close(fd[STDIN_FILENO]);
-			close(fd[STDOUT_FILENO]);
-			printf("Top and echo launched (%d) (%d)\n", pid1, pid2);
-			printf("Just waiting!!!!\n");
-			wait_for_pid("Waiting por top", pid1);
-			wait_for_pid("Waiting for echo", pid2);
-		}
-		else
-		{
-			close(fd[STDOUT_FILENO]);
-			dup2(fd[STDIN_FILENO], STDIN_FILENO);
-			close(fd[STDIN_FILENO]);
-			int status = wait_for_pid("Echo child", pid1);
-			printf("top status %d\n",  status);
-			exit(0);
-		}
+	signal(SIGINT, signals);
+
+	pipe(fd);
+	pid1 = fork();
+	if (!pid1)
+	{
+		printf("Top thread child\n");
+		close(fd[STDIN_FILENO]);
+		dup2(fd[STDOUT_FILENO], STDOUT_FILENO);
+		close(fd[STDOUT_FILENO]);
+		char *argv[2];
+		argv[0] = "top";
+		argv[1] = 0;
+		// top
+		execve("/usr/bin/top", argv, envp);
+		int fd = open("./test_result", O_RDWR | O_CREAT | O_TRUNC, 0666);
+		write(fd, strerror(errno), strlen(strerror(errno)));
+		close(fd);
+		exit(1);
+	}
+
+	pipe(fd23);
+	pid2 = fork();
+	if (!pid2)
+	{
+		close(fd[STDOUT_FILENO]);
+		dup2(fd[STDIN_FILENO], STDIN_FILENO);
+		close(fd[STDIN_FILENO]);
+
+		close(fd23[STDIN_FILENO]);
+		dup2(fd23[STDOUT_FILENO], STDOUT_FILENO);
+		close(fd23[STDOUT_FILENO]);
+
+		int status = wait_for_pid("Echo child", pid1);
+
+		printf("top status %d %s\n",  status, strerror(errno));
+		exit(0);
+	}
+
+	pid3 = fork();
+	if (pid3)
+	{
+		close(fd[STDIN_FILENO]);
+		close(fd[STDOUT_FILENO]);
+		close(fd23[STDIN_FILENO]);
+		close(fd23[STDOUT_FILENO]);
+
+		printf("Top and echo launched (%d) (%d) (%d)\n", pid1, pid2, pid3);
+		printf("Just waiting!!!!\n");
+		wait_for_pid("Waiting por top", pid1);
+		wait_for_pid("Waiting for echo", pid2);
+		wait_for_pid("Waiting for ls", pid3);
+
+		dup2(infile, STDIN_FILENO);
+		dup2(outfile, STDOUT_FILENO);
+		dup2(errfile, STDERR_FILENO);
+		close(infile);
+		close(outfile);
+		close(errfile);
+	}
+	else
+	{
+		close(fd23[STDOUT_FILENO]);
+		dup2(fd23[STDIN_FILENO], STDIN_FILENO);
+		close(fd23[STDIN_FILENO]);
+
+		char *argv[2];
+		argv[0] = "ls";
+		argv[1] = 0;
+		// ls
+		execve("/bin/ls", argv, envp);
+		exit(1);
+	}
 
 }
