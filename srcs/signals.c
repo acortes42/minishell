a263d0430d253e73cb6e6404ea9293a14866f182
visv <victor.sempere1@gmail.com>
2021-05-22 10:59:43 +0200

    Ajustados códigos de status con Ctrl + c

--- srcs/signals.c
+++ srcs/signals.c
@@ -15,13 +15,6 @@
 void	signal_handler(int sig)
 {
 	extern t_abs_struct	g_base;
-	char *a;
-
-	a = ft_itoa(sig);
-	ft_putstr_fd("Signal ", STDERR_FILENO);
-	ft_putstr_fd(a, STDERR_FILENO);
-	ft_putstr_fd("\n", STDERR_FILENO);
-	free(a);
 
 	g_base.last_status = 128 + sig;
 	if (sig == SIGINT)
@@ -29,6 +22,7 @@ void	signal_handler(int sig)
 		ft_memset(g_base.input_bf, 0, BUFFER_SIZE);
 		if (g_base.input)
 			free(g_base.input);
+		g_base.last_status = 1;
 		g_base.input = 0;
 		ft_putstr("^C\n");
 		ft_show_prompt(&g_base);
@@ -45,17 +39,16 @@ void	forked_process_signal_handler(int sig)
 
 	if (sig == SIGINT || sig == SIGQUIT)
 	{
-		if (sig == SIGINT)
-			ft_putstr("\n");
-		else if (sig == SIGQUIT)
-			ft_putstr_fd("Abandona (`core' generado)\n", STDERR_FILENO);
 		g_base.last_status = 128 + sig;
-		char *a;
-
-		a = ft_itoa(sig);
-		ft_putstr_fd("Signal ", STDERR_FILENO);
-		ft_putstr_fd(a, STDERR_FILENO);
-		ft_putstr_fd("\n", STDERR_FILENO);
-		free(a);
+		if (sig == SIGINT)
+		{
+			ft_putstr("\n");
+			g_base.last_status = 1;
+		}
+		else if (sig == SIGQUIT)
+		{
+			ft_putstr_fd("Abandona (`core' generado)\n", STDERR_FILENO);
+			g_base.last_status = 0;
+		}
 	}
 }
